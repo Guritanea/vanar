82eb9da57a498ed7fd80c94a2fe1451a5db331a8
Guritanea <73936476+Guritanea@users.noreply.github.com>
2020-11-09 13:46:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
-function capitalize(text) {
-    return text[0].toUpperCase() + text.substring(1)
+////////Капитализация заданного текста
+    function capitalize(text){
+        return text[0].toUpperCase() + text.substring(1)
+    }
 
-}
+///////текст сообщения об ошибке, тип компонента
+    var errorMsg = {
+        msg: "",
+        type: ""
+    }
 
-function loadComponent(name) {
+//////////Загрузка  указанного компонента
+    function loadComponent(name){
+
         let link = document.createElement('link')
-    link.rel = "stylesheet"
-    link.href = `src/${name}/${name}.css`
-    document.head.appendChild(link)
-   
-    let script = document.createElement('script')
-    script.src = `src/${name}/${capitalize(name)}.js`
-    document.body.appendChild(script)
-}
+            link.rel = "stylesheet"
+            link.href = `src/${name}/${name}.css`
+            errorMsg.type = name
+            link.addEventListener( 'error',  errorAlert ) // добавлено
+            //link.addEventListener( 'error', function() { alert(  `\nERROR!\n Cannot load ${name}.css component`) } )
+        document.head.appendChild(link)  
 
-var msg=""
-let t=null
-   window.addEventListener('DOMContentLoaded', (event) => { 
-            document.addEventListener('error', function (e) {
-                msg+=`Cannot load ${name}  ${e.target.tagName}\n`
-                }, true) 
-                clearTimeout(t)
-                t=setTimeout(showError, 1000)
-            })
+        let script = document.createElement('script')
+            script.src = `src/${name}/${capitalize(name)}.js`
+            errorMsg.type = name
+            script.addEventListener( 'error',  errorAlert ) // добавлено
+            //script.addEventListener( 'error', function() { alert(  `\nERROR!\n Cannot load ${name}.js component`) } )         
+        document.body.appendChild(script)    
+    
+    }
 
-function showError(){
-        alert(`ERROR!!! ${msg}`)
-        }
+////////формирование сообщения добавлено
+    function errorAlert(e){
+        if (e.target.tagName.toLowerCase() == "link")   errorMsg.msg += `\n Cannot load ${errorMsg.type}.css component`;   
+        else if (e.target.tagName.toLowerCase() == "script") errorMsg.msg += `\n Cannot load ${errorMsg.type}.js component`;   
+    }
 
+loadComponent("user")
+loadComponent("search")
 
-///////
-loadComponent("ser")
+//////////Вывод потенциального сообщения об ошибке
+    window.addEventListener("load", () => {   
+        if (errorMsg.msg != "")    alert(errorMsg.msg)
+    })
+
+    window.onload = function(){
+        console.log('WINDOW LOADED!!!')
+
+        testFakeUser()
+    }
